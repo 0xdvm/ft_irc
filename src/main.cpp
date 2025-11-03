3db52f99284b9a8314f7c8486989c5a7b7bb7b84
Daniel Vemba <dvemba@c2r1s2.42luanda.com>
2025-11-03 18:28:51 +0100

    refactor(validation): Melhorando a validação principal do arquivo main.cpp.

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marcsilv <marcsilv@42.student.fr>          +#+  +:+       +#+        */
+/*   By: dvemba <dvemba@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/01 19:28:42 by marcsilv          #+#    #+#             */
-/*   Updated: 2025/11/03 15:49:52 by marcsilv         ###   ########.fr       */
+/*   Updated: 2025/11/03 17:18:35 by dvemba           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,12 @@
 #include <stdexcept>
 
 int	main(int ac, char **av) {
+    /**/
+    if (ac != 3){
+        std::cout << "Wrong args\nRun: ./ircserv <port> <password>\n";
+        return (1);
+    }    
 	try {
-		if (ac != 3)
-			throw std::runtime_error ("Wrong args\nRun: ./ircserv <port> <password>\n");
 		IRC irc(av[1], av[2]);
         irc.run();
 	} catch (const std::exception &e) {
