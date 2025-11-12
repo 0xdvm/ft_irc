47d02d0c45f915519d619e2849b40a9df1e36b89
Daniel Josias <danieljosias693@gmail.com>
2025-11-12 21:22:26 +0100

    feet(Class PASS): Criando a lógica do comando PASS e verificando se já enviou a PASS correcta.

--- inc/commands/PASS.hpp
+++ inc/commands/PASS.hpp
@@ -1,31 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Command.hpp                                        :+:      :+:    :+:   */
+/*   PASS.hpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dvemba <dvemba@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/11/11 10:46:35 by dvemba            #+#    #+#             */
-/*   Updated: 2025/11/12 21:19:41 by dvemba           ###   ########.fr       */
+/*   Created: 2025/11/12 12:20:04 by dvemba            #+#    #+#             */
+/*   Updated: 2025/11/12 16:01:50 by dvemba           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef COMMAND_HPP
-#define COMMAND_HPP
+#ifndef PASS_HPP
+#define PASS_HPP
 
-#include <iostream>
-#include <vector>
-#include "../Server.hpp"
-#include "../Client.hpp"
+#include "Command.hpp"
 
-class Command{
+class PASS: public Command{
   private:
-    const int num_args;
+  
   public:
-    Command(int num_args);
-    virtual ~Command();
-    
-    virtual void run_command(Server& server_ref, Client& client_ref, std::vector<std::string> args) const = 0;
-    int get_num_args() const;
+    PASS();
+    ~PASS();
+    void run_command(Server& server_ref, Client& client_ref, std::vector<std::string> args) const;
 };
 #endif
