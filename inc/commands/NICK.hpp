87d9ea504093c78da9f8aeec452330868e792e6e
Daniel Josias <danieljosias693@gmail.com>
2025-11-14 21:26:00 +0100

    feet(NICK): Implementando a classe NICK que lida com o comando NICK do servidor irc.

---
+++ inc/commands/NICK.hpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   NICK.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dvemba <dvemba@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/14 19:21:24 by dvemba            #+#    #+#             */
+/*   Updated: 2025/11/14 20:49:53 by dvemba           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef NICK_HPP
+#define NICK_HPP
+
+#include "Command.hpp"
+
+class NICK: public Command{
+    public:
+        NICK();
+        ~NICK();
+        void run_command(Server& server_ref, Client& client_ref, std::vector<std::string> args);
+        bool isvalidNickname(std::string nickname);
+        bool isFreeNick(Server& server_ref, std::string nickname);
+};
+#endif
