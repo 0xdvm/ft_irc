ba57a1eabd635f733f9f172a2107e1721d3f642c
Daniel Josias <danieljosias693@gmail.com>
2025-11-14 21:32:38 +0100

    fix(CAP): Corrigindo o erro de conflito de implementação da função puramente virtual

--- inc/commands/CAP.hpp
+++ inc/commands/CAP.hpp
@@ -6,7 +6,7 @@
 /*   By: dvemba <dvemba@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/13 20:26:41 by dvemba            #+#    #+#             */
-/*   Updated: 2025/11/13 20:30:59 by dvemba           ###   ########.fr       */
+/*   Updated: 2025/11/14 21:30:09 by dvemba           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,6 @@ class CAP: public Command{
     public:
         CAP();
         ~CAP();
-        void run_command(Server& server_ref, Client& client_ref, std::vector<std::string> args) const;
+        void run_command(Server& server_ref, Client& client_ref, std::vector<std::string> args);
 };
 #endif
