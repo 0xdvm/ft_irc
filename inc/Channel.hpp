a93dd97aee0702d36557d904e555ac3398ffb984
Daniel Josias <danieljosias693@gmail.com>
2025-11-24 21:22:31 +0100

    feet(Channel): Criando o método sendBroadcast para enviar mensagem para todos do canal e outros metodos auxiliares.

--- inc/Channel.hpp
+++ inc/Channel.hpp
@@ -6,7 +6,7 @@
 /*   By: dvemba <dvemba@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/20 11:28:37 by dvemba            #+#    #+#             */
-/*   Updated: 2025/11/24 15:15:39 by dvemba           ###   ########.fr       */
+/*   Updated: 2025/11/24 20:59:02 by dvemba           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,21 +23,27 @@ class Channel{
         std::string                 _password;
         std::string                 _topic;
         std::list<std::string>      _operatorsList;
-        std::list<Client>      _memberList;
+        std::list<Client>           _memberList;
         std::list<std::string>      _bannedList;
         int                         _memberNum;
+
+        bool                        _hasPassword;
     public:
         Channel();
         Channel(std::string channelName);
+        Channel(std::string channelName, std::string password);
         ~Channel();
         
-        void joinChannel(Client&);
+        void            joinChannel(Client&);
         void            addOperator(std::string);
         void            addMember(Client&);
-        std::string     getChannelName();
-
+        void            sendBroadcast(std::string command, std::string tosend, Client& client);
+        
         void            setMemberNum(int num);
         int             getMemberNum();
         bool            isMember(std::string nickname);
+        bool            hasPassword();
+        bool            isCorrectpassword(std::string password);
+        std::string     getChannelName();
 };
 #endif
--- src/Channel.cpp
+++ src/Channel.cpp
@@ -6,15 +6,18 @@
 /*   By: dvemba <dvemba@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/20 11:28:28 by dvemba            #+#    #+#             */
-/*   Updated: 2025/11/24 15:16:58 by dvemba           ###   ########.fr       */
+/*   Updated: 2025/11/24 21:08:30 by dvemba           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/Channel.hpp"
+#include "../inc/utils.hpp"
 
 Channel::Channel() : _channelName("") {}
 
-Channel::Channel(std::string channelName):_channelName(channelName){}
+Channel::Channel(std::string channelName):_channelName(channelName), _password(""), _topic(""), _memberNum(0), _hasPassword(false){}
+
+Channel::Channel(std::string channelName, std::string password):_channelName(channelName), _password(password), _topic(""), _memberNum(0), _hasPassword(true){}
 
 Channel::~Channel(){}
 
@@ -25,10 +28,13 @@ void Channel::joinChannel(Client& client){
     if (this->getMemberNum() == 1){
         this->addOperator(client.getNickname());
     }
+    std::string tosend = "has joined " + this->getChannelName();
+    this->sendBroadcast("JOIN", tosend, client);
 }
 
 void Channel::addMember(Client& client){
     this->_memberList.push_back(client);
+    this->_memberNum++;
 }
 
 void Channel::addOperator(std::string nickname){
@@ -55,6 +61,30 @@ bool Channel::isMember(std::string nickname){
             return (true);
         }
         it++;
+    }
+    return (false);
+}
+
+bool Channel::hasPassword()
+{
+    return (this->_hasPassword);
+}
+
+void Channel::sendBroadcast(std::string command, std::string tosend, Client& client)
+{
+    std::list<Client>::iterator it = this->_memberList.begin();
+
+    while (it != this->_memberList.end()){
+        send_irc_reply(*it, client.userMask(), command, this->getChannelName(), tosend);
+        it++;
+    }
+}
+
+bool Channel::isCorrectpassword(std::string password)
+{
+    if (this->_password.compare(password) == 0)
+    {
+        return (true);
     }
     return (false);
 }
