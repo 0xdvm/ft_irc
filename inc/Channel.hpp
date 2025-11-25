92b738fb24dec2c78f89150c88bd4c9ba12052fd
Daniel Josias <danieljosias693@gmail.com>
2025-11-25 20:36:55 +0100

    feet(Channel): Reformula a forma que os dados são guardados numa lista em mapa e adiciona alguns métodos necessários.

--- inc/Channel.hpp
+++ inc/Channel.hpp
@@ -6,7 +6,7 @@
 /*   By: dvemba <dvemba@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/20 11:28:37 by dvemba            #+#    #+#             */
-/*   Updated: 2025/11/24 20:59:02 by dvemba           ###   ########.fr       */
+/*   Updated: 2025/11/25 17:30:32 by dvemba           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,21 @@
 
 #include <iostream>
 #include <list>
+#include <map>
 #include "Client.hpp"
 
 class Channel{
     private:
-        std::string                 _channelName;
-        std::string                 _password;
-        std::string                 _topic;
-        std::list<std::string>      _operatorsList;
-        std::list<Client>           _memberList;
-        std::list<std::string>      _bannedList;
-        int                         _memberNum;
+        std::string                             _channelName;
+        std::string                             _password;
+        std::string                             _topic;
+        // std::list<std::string>      _operatorsList;
+        std::map<std::string, Client>           _memberList;
+        std::list<std::string>                  _bannedList;
+        int                                     _memberNum;
 
-        bool                        _hasPassword;
+        bool                                    _hasPassword;
+        bool                                    _hasTopic;
     public:
         Channel();
         Channel(std::string channelName);
@@ -35,15 +37,18 @@ class Channel{
         ~Channel();
         
         void            joinChannel(Client&);
-        void            addOperator(std::string);
-        void            addMember(Client&);
+        bool            addOperator(std::string);
+        void            addMember(std::string nickname, Client&);
         void            sendBroadcast(std::string command, std::string tosend, Client& client);
+        void            setTopic(std::string topic);
         
         void            setMemberNum(int num);
         int             getMemberNum();
         bool            isMember(std::string nickname);
         bool            hasPassword();
+        bool            hasTopic();
         bool            isCorrectpassword(std::string password);
         std::string     getChannelName();
+        std::string     getListmember();
 };
 #endif
--- src/Channel.cpp
+++ src/Channel.cpp
@@ -6,7 +6,7 @@
 /*   By: dvemba <dvemba@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/20 11:28:28 by dvemba            #+#    #+#             */
-/*   Updated: 2025/11/24 21:08:30 by dvemba           ###   ########.fr       */
+/*   Updated: 2025/11/25 17:45:27 by dvemba           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,37 +15,60 @@
 
 Channel::Channel() : _channelName("") {}
 
-Channel::Channel(std::string channelName):_channelName(channelName), _password(""), _topic(""), _memberNum(0), _hasPassword(false){}
+Channel::Channel(std::string channelName):_channelName(channelName), _password(""), _topic(""), _memberNum(0), _hasPassword(false), _hasTopic(false){}
 
-Channel::Channel(std::string channelName, std::string password):_channelName(channelName), _password(password), _topic(""), _memberNum(0), _hasPassword(true){}
+Channel::Channel(std::string channelName, std::string password):_channelName(channelName), _password(password), _topic(""), _memberNum(0), _hasPassword(true), _hasTopic(false){}
 
 Channel::~Channel(){}
 
-void Channel::joinChannel(Client& client){
+void Channel::joinChannel(Client& client)
+{
     
-    this->addMember(client);
+    this->addMember(client.getNickname(), client);
     
-    if (this->getMemberNum() == 1){
+    if (this->getMemberNum() == 1)
+    {
         this->addOperator(client.getNickname());
     }
     std::string tosend = "has joined " + this->getChannelName();
     this->sendBroadcast("JOIN", tosend, client);
+    // this->sendBroadcast()
 }
 
-void Channel::addMember(Client& client){
-    this->_memberList.push_back(client);
+void Channel::addMember(std::string nickname, Client& client)
+{
+    this->_memberList[nickname] =  client;
     this->_memberNum++;
 }
 
-void Channel::addOperator(std::string nickname){
-    this->_operatorsList.push_back(nickname);
+bool Channel::addOperator(std::string nickname)
+{
+    Client client;
+    
+    std::map<std::string, Client>::iterator it = this->_memberList.begin();
+
+    while (it != this->_memberList.end()){
+
+        if (it->second.getNickname().compare(nickname) == 0)
+        {
+            client = it->second;
+            this->_memberList.erase(it->first);
+            
+            this->_memberList["@" + nickname] = client;
+            return (true);
+        }
+        it++;
+    }
+    return(false);
 }
 
-void Channel::setMemberNum(int num){
+void Channel::setMemberNum(int num)
+{
     this->_memberNum = num;
 }
 
-int Channel::getMemberNum(){
+int Channel::getMemberNum()
+{
     return (this->_memberNum);
 }
 
@@ -53,15 +76,24 @@ std::string Channel::getChannelName(){
     return (this->_channelName);
 }
 
-bool Channel::isMember(std::string nickname){
-    std::list<Client>::iterator it = this->_memberList.begin();
-
-    while (it != this->_memberList.end()){
-        if (it->getNickname() == nickname){
-            return (true);
-        }
-        it++;
+bool Channel::isMember(std::string nickname)
+{
+    std::map<std::string, Client>::iterator it;
+    
+    it = this->_memberList.find(nickname);
+    
+    if (it != this->_memberList.end())
+    {
+        return (true);
     }
+    // std::map<Client>::iterator it = this->_memberList.begin();
+
+    // while (it != this->_memberList.end()){
+    //     if (it->getNickname() == nickname){
+    //         return (true);
+    //     }
+    //     it++;
+    // }
     return (false);
 }
 
@@ -70,12 +102,18 @@ bool Channel::hasPassword()
     return (this->_hasPassword);
 }
 
+bool Channel::hasTopic()
+{
+    return (this->_hasTopic);
+}
+
 void Channel::sendBroadcast(std::string command, std::string tosend, Client& client)
 {
-    std::list<Client>::iterator it = this->_memberList.begin();
+    std::map<std::string, Client>::iterator it = this->_memberList.begin();
 
-    while (it != this->_memberList.end()){
-        send_irc_reply(*it, client.userMask(), command, this->getChannelName(), tosend);
+    while (it != this->_memberList.end())
+    {
+        send_irc_reply(it->second, client.userMask(), command, this->getChannelName(), tosend);
         it++;
     }
 }
@@ -87,4 +125,31 @@ bool Channel::isCorrectpassword(std::string password)
         return (true);
     }
     return (false);
+}
+
+void  Channel::setTopic(std::string topic)
+{
+    if (topic.empty())
+    {
+        this->_topic = topic;
+        this->_hasTopic = false;
+        return;
+    }
+    this->_topic = topic;
+    this->_hasTopic = true;
+}
+
+std::string Channel::getListmember(){
+    std::map<std::string, Client>::iterator it = this->_memberList.begin();
+    std::string list;
+    
+    list = it->first;
+    it++;
+    while(it != this->_memberList.end())
+    {
+        list.append(" " + it->first);
+        it++;
+    }
+    std::cout << "LIST OF: " << list << std::endl;
+    return (list);
 }
