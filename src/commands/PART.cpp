0c45dbd0653c404c3a806fe436fa35a6e49a5146
CostantinoGouveia <costantino.gouveia938@gmail.com>
2025-12-17 15:16:34 +0100

    feet(QUIT,, PART): para permitir retirar tambm da lista de convidados quando eles saiem do canal

--- src/commands/PART.cpp
+++ src/commands/PART.cpp
@@ -1,13 +1,12 @@
-<<<<<<< HEAD
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   PART.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dvemba <dvemba@student.42.fr>              +#+  +:+       +#+        */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/09 19:05:29 by cgouveia          #+#    #+#             */
-/*   Updated: 2025/12/13 12:36:24 by dvemba           ###   ########.fr       */
+/*   Updated: 2025/12/17 15:08:57 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,6 +48,7 @@ void PART::run_command(Server& server_ref, Client& client_ref, std::vector<std::
             // send_irc_reply(client_ref, client_ref.userMask(), "PART", channel.getChannelName(), tosend);
             channel.sendBroadcast("PART", "", client_ref, true);
             channel.removeMember(client_ref.getNickname());
+            channel.removeInviteList(client_ref.getNickname());
             if (channel.getListmember().empty())
             {
                 server_ref.removeChannel(channel.getChannelName());
@@ -76,6 +76,7 @@ void PART::run_command(Server& server_ref, Client& client_ref, std::vector<std::
             // send_irc_reply(client_ref, client_ref.userMask(), "PART", channel.getChannelName(), tosend);
             channel.sendBroadcast("PART", args[1], client_ref, true);
             channel.removeMember(client_ref.getNickname());
+            channel.removeInviteList(client_ref.getNickname());
             if (channel.getListmember().empty())
             {
                 server_ref.removeChannel(channel.getChannelName());
@@ -92,98 +93,3 @@ void PART::run_command(Server& server_ref, Client& client_ref, std::vector<std::
     
     
 }
-=======
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   PART.cpp                                           :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: cgouveia <marvin@42.fr>                    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/12/09 19:05:29 by cgouveia          #+#    #+#             */
-/*   Updated: 2025/12/09 19:05:34 by cgouveia         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
-#include "../../inc/commands/PART.hpp"
-#include "../../inc/utils.hpp"
-#include "../../inc/Channel.hpp"
-
-PART::PART(): Command(2){}
-
-PART::~PART(){}
-
-void PART::run_command(Server& server_ref, Client& client_ref, std::vector<std::string> args)
-{
-    int size_args = args.size();
-    std::string target = client_ref.getNickname();
-
-    if (size_args == 0)
-    {
-        send_irc_reply(client_ref, server_ref.get_Servername(), ERR_NEEDMOREPARAMS, target, "Not enough paramters");
-        return;
-    }
-    if (size_args >= 1 && args[0].at(0) != '#')
-    {
-        send_irc_reply(client_ref, server_ref.get_Servername(), ERR_NOSUCHCHANNEL, target, "No such channel");
-        return;
-    }
-
-    if (size_args == 1)
-    {
-        try
-        {
-            Channel& channel = server_ref.findChannel(args[0]);
-            if (!channel.isMember(client_ref.getNickname()))
-            {
-                send_irc_reply(client_ref, server_ref.get_Servername(), ERR_NOTONCHANNEL, target + " " + channel.getChannelName(), "You're not on that channel");
-                return;
-            }
-            std::string tosend = "has left " + channel.getChannelName();
-            // send_irc_reply(client_ref, client_ref.userMask(), "PART", channel.getChannelName(), tosend);
-            channel.sendBroadcast("PART", "", client_ref, true);
-            channel.removeMember(client_ref.getNickname());
-            if (channel.getListmember().empty())
-            {
-                server_ref.removeChannel(channel.getChannelName());
-            }
-            
-        }
-        catch(const std::exception& e)
-        {
-            send_irc_reply(client_ref, server_ref.get_Servername(), ERR_NOSUCHCHANNEL, target + " " + args[0], "No such channel");
-            return;
-        }
-    }
-
-    if (size_args > 1)
-    {
-        try
-        {
-            Channel& channel = server_ref.findChannel(args[0]);
-            if (!channel.isMember(client_ref.getNickname()))
-            {
-                send_irc_reply(client_ref, server_ref.get_Servername(), ERR_NOTONCHANNEL, target + " " + channel.getChannelName(), "You're not on that channel");
-                return;
-            }
-            std::string tosend = "has left " + channel.getChannelName();
-            // send_irc_reply(client_ref, client_ref.userMask(), "PART", channel.getChannelName(), tosend);
-            channel.sendBroadcast("PART", args[1], client_ref, true);
-            channel.removeMember(client_ref.getNickname());
-            if (channel.getListmember().empty())
-            {
-                server_ref.removeChannel(channel.getChannelName());
-            }
-            
-        }
-        catch(const std::exception& e)
-        {
-            send_irc_reply(client_ref, server_ref.get_Servername(), ERR_NOSUCHCHANNEL, target + " " + args[0], "No such channel");
-            return;
-        }
-    }
-    
-    
-    
-}
->>>>>>> main
